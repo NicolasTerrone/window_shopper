4af4d9c106db70ad99e04dc040e0dafa81ffa40d
Nicolas TERRONE <nicolasterrone@gmail.com>
2019-01-21 09:48:49 +0100

    Made Custom text field @IBDesignable, updated User Interface

--- window_shopper/View/CurrencyTxtField.swift
+++ window_shopper/View/CurrencyTxtField.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTxtField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeTextField()
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeTextField()
+    }
+    
+    func customizeTextField(){
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         
         if let place = placeholder {
-            self.attributedPlaceholder = NSAttributedString(string: place, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = NSAttributedString(string: place, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
         }
     }
 
