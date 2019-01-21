63db0b65752a69a4774cb537c937433265ce047a
Nicolas TERRONE <nicolasterrone@gmail.com>
2019-01-21 10:12:27 +0100

    Added custom inputBtn to default keyboard and finalized User Interface

--- window_shopper/Controller/MainVC.swift
+++ window_shopper/Controller/MainVC.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calculateBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calculateBtn.backgroundColor = #colorLiteral(red: 1, green: 0.2597229183, blue: 0, alpha: 1)
+        calculateBtn.setTitle("Calculate", for: .normal)
+        calculateBtn.tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        calculateBtn.addTarget(self, action: #selector(MainVC.calcultate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calculateBtn
+        priceTxt.inputAccessoryView = calculateBtn
+    }
+    
+    @objc func calcultate(){
+        
     }
 
 
 }
+
 
